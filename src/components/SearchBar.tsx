d0444ee90a4f8a625a07a15c78e521ca6366e660
Demian <aimedemian@gmail.com>
2023-12-18 12:01:45 -0300

    search bar funciona con estado. no optima

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 // "use client";
 import axios from "axios";
 // import Image from "next/image";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { TbLoaderQuarter } from "react-icons/tb";
 
 interface SearchBarProps {
@@ -19,29 +19,25 @@ export default function SearchBar({
   setResults,
   results,
 }: SearchBarProps) {
-  const [inputValueRef, setInputValueRef] = useState("");
+  const [inputHeroEntry, setInputHeroEntry] = useState<string | null>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
 
-  async function searchHeroes() {
-    const apiHeroes = `https://superheroapi.com/api.php/${accessToken}/search/${inputValueRef}`;
+  useEffect(() => {
+    async function searchHeroes() {
+      const apiHeroes = `https://superheroapi.com/api.php/${accessToken}/search/${inputHeroEntry}`;
 
-    setIsLoading(true);
-    let res = await axios.get(apiHeroes);
-    setIsLoading(false);
-    if (res.data.results) {
-      setResults(res.data.results);
-    } else {
-      setResults([]);
+      setIsLoading(true);
+      let res = await axios.get(apiHeroes);
+      setIsLoading(false);
+      if (res.data.results) {
+        setResults(res.data.results);
+      } else {
+        setResults([]);
+      }
     }
-    console.log(res.data.results);
-  }
-
-  //ver como hacer que esto funcione
-  // function setAndSearchByValue(e) {
-  //   setInputValueRef(e.target.value);
-  //   () => searchHeroes();
-  // }
-
-  // console.log(inputValueRef);
+    searchHeroes();
+  }, [accessToken, inputHeroEntry, setIsLoading, setResults]);
+  // console.log(results);
 
   return (
     <>
@@ -50,46 +46,26 @@ export default function SearchBar({
           type="text"
           placeholder="Type some character to search for a hero"
           className="w-full outline-none"
-          value={inputValueRef}
-          onChange={(e) => setInputValueRef(e.target.value)}
-          onKeyDown={(e) =>
-            e.keyCode === "8" ||
-            e.keyCode === "32" ||
-            (e.keyCode > 47 && e.keyCode < 91) ||
-            (e.keyCode > 93 && e.keyCode < 106) ||
-            e.keyCode > 185
-              ? searchHeroes()
-              : ""
-          }
-          // esto anda como el orto. revisar
-
-          // {(e) => searchHeroes()}
+          onChange={(e) => setInputHeroEntry(e.target.value)}
         />
-        {/* <Image
-          className="hover:scale-110 cursor-pointer"
-          src="/assets/loupe.png"
-          alt="Landscape picture"
-          width={30}
-          height={15}
-          onClick={searchHeroes}
-        /> */}
       </div>
       <ul className="w-10/12 max-h-60 overflow-auto">
         {isLoading ? (
           <TbLoaderQuarter className="animate-spin p-1" />
-        ) : results.length >= 1 ? (
-          results.map((hero) => (
-            <li
-              className="border-x border-b p-1 hover:bg-blue-200"
-              key={hero.id}
-            >
-              {hero.name}
-            </li>
-          ))
-        ) : (
-          //intentar que no se vea en el primer render
-          <li className="italic p-1 text-xs text-gray-700">No results</li>
-        )}
+        ) : results ? (
+          results.length < 1 ? (
+            <li className="italic p-1 text-xs text-gray-700">No results</li>
+          ) : (
+            results.map((hero) => (
+              <li
+                className="border-x border-b p-1 hover:bg-blue-200"
+                key={hero.id}
+              >
+                {hero.name}
+              </li>
+            ))
+          )
+        ) : null}
       </ul>
     </>
   );
