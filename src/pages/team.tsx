b58e2fb149ec248fd45d0fb4b33cc9e52a97d995
demaime~ <aimedemian@gmail.com>
2024-01-04 20:00:10 -0300

    funciona el map de heroes por id, pero esta mal
    Co-authored-by: Guillermo Maiolo <guille.maiolo@gmail.com>

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { Hero } from "@/types/hero-type";
+
+// * obtener ids de la url
+// * en el primer render, agarrar los ids y traer de la api de superhero cada uno
+//   * tiparlo o traer el tipo
+// * con el resultado, setear un estado con los heroes que obtuve
+// * en el jsx, mappear por los heroes que obtuve (estado) y mostrar los nombres
 
 export default function Team() {
+  const accessToken = 6728050277235129;
   const router = useRouter();
-  const teamIds = router.query.ids?.toString().split(",") || [];
+  const [team, setTeam] = useState<Hero[]>([]);
 
-  console.log(teamIds);
+  useEffect(() => {
+    const teamIds = router.query.ids?.toString().split(",") || [];
+    async function getHeroesByID() {
+      for (const id of teamIds) {
+        const { data } = await axios.get<Hero>(
+          `https://superheroapi.com/api.php/${accessToken}/${id}`
+        );
+
+        setTeam((prev) => [...prev, data]);
+      }
+    }
+    getHeroesByID();
+  }, [router.query.ids]);
 
   return (
     <div id="full-container" className="bg-gray-900">
       <Header />
-      <ul className="w-full h-11/12 flex flex-col items-center justify-evenly">
-        <li className="bg-gray-300 w-11/12 text-center h-16 ">HERO</li>
-        <li
-          onClick={() => console.log(teamIds)}
-          className="bg-gray-300 w-11/12 text-center h-16 "
-        >
-          HERO
-        </li>
-        <li className="bg-gray-300 w-11/12 text-center h-16 ">HERO</li>
-        <li className="bg-gray-300 w-11/12 text-center h-16 ">HERO</li>
-        <li className="bg-gray-300 w-11/12 text-center h-16 ">HERO</li>
-        <li className="bg-gray-300 w-11/12 text-center h-16 ">HERO</li>
+      <ul className="w-full h-11/12 flex flex-col items-center justify-evenly text-white">
+        {team.map((hero) => (
+          <li key={hero.name}>{hero.name}</li>
+        ))}
       </ul>
     </div>
   );
