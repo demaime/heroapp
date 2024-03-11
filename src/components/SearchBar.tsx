94c28ba8699bb7fbeef61088c6e23b64f1bcb72a
Demian <aimedemian@gmail.com>
2024-03-11 10:38:20 -0300

    test sin debounce

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -26,16 +26,16 @@ export default function SearchBar({
   const [ulResultsVisibility, setUlResultsVisibility] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // Función debounce para retrasar la llamada a searchHeroes
-  const debounce = (func: Function, delay: number) => {
-    let timer: ReturnType<typeof setTimeout>;
-    return function (this: any, ...args: any[]) {
-      clearTimeout(timer);
-      timer = setTimeout(() => func.apply(this, args), delay);
-    };
-  };
+  
+  // const debounce = (func: Function, delay: number) => {
+  //   let timer: ReturnType<typeof setTimeout>;
+  //   return function (this: any, ...args: any[]) {
+  //     clearTimeout(timer);
+  //     timer = setTimeout(() => func.apply(this, args), delay);
+  //   };
+  // };
 
-  const debouncedSearchHeroes = debounce(searchHeroes, 300); // 300ms de retraso
+  // const debouncedSearchHeroes = debounce(searchHeroes, 300); 
 
   async function searchHeroes() {
     const apiHeroes = `https://superheroapi.com/api.php/${accessToken}/search/${inputRef.current?.value}`;
